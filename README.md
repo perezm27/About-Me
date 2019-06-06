96dfdc060dc6d7d4e40b3befa3d291d7d6e5a27c
MELFI PEREZ <perezmelfi@outlook.com>
2019-06-05 19:59:29 -0700

    Added two more questions and styling was updated

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # About-Me
-A website about me
+Author: Melfi Perez
+
+A website designed to interactively get users to get to know you
+while playing a mini game.
+
+In this version:
+Question 6 and 7 where added which gave the users the ability to try to guess the answers with limited tries
+
+Styling for the website was also updated to include a sticky footer
+and ordered and unordered lists were also added to the HTML.
--- js/app.js
+++ js/app.js
@@ -1,16 +1,20 @@
 'use strict';
 
+//sets counter that will keep track of questions answered correctly
+var counter = 0;
+
 //Begining of Minigame
 var username = prompt('What is your name?');
 var usernameMessage = 'Hey there ' + username + ' welcome to my site! I am going to be asking you a series of Yes or No questions. Have fun!';
 alert(usernameMessage);
 
-
+//Question 1
 var favoriteColor = prompt('Is my favorite color black?');
 favoriteColor = favoriteColor.toLowerCase(); //Accepts User input regardless of case
 
 if(favoriteColor === 'yes' || favoriteColor === 'y'){
   alert('Awesome! How did you know?');
+  counter++;
 } else if (favoriteColor === 'no' || favoriteColor === 'n'){
   alert('Sorry that is not correct :(');
 } else {
@@ -18,12 +22,13 @@ if(favoriteColor === 'yes' || favoriteColor === 'y'){
 }
 console.log('FavoriteColor: ' + favoriteColor);
 
-
+//Question 2
 var moveThings = prompt('Can I move things with my mind?');
 moveThings = moveThings.toLowerCase();
 
 if(moveThings === 'no' || moveThings === 'n'){
   alert('Awesome! How did you know?');
+  counter++;
 } else if (moveThings === 'yes' || moveThings === 'y'){
   alert('Sorry that is not correct :(');
 } else {
@@ -31,12 +36,13 @@ if(moveThings === 'no' || moveThings === 'n'){
 }
 console.log('moveThings: ' + moveThings);
 
-
+//Question 3
 var jediMaster = prompt('Am I a Jedi Master?');
 jediMaster = jediMaster.toLowerCase();
 
 if(jediMaster=== 'no' || jediMaster === 'n'){
   alert('You are right >:p, The darkside is strong with this one');
+  counter++;
 } else if (jediMaster === 'yes' || jediMaster === 'y'){
   alert('Sorry that is not correct; The force is not strong with you');
 } else {
@@ -44,12 +50,13 @@ if(jediMaster=== 'no' || jediMaster === 'n'){
 }
 console.log('jediMaster: ' + jediMaster);
 
-
+//Question 4
 var pet = prompt('Do I have any pets?');
 pet = pet.toLowerCase();
 
 if(pet === 'yes' || pet === 'y'){
   alert('Awesome! How did you know?');
+  counter++;
 } else if (pet === 'no' || pet === 'n'){
   alert('Sorry that is not correct :(');
 } else {
@@ -57,12 +64,13 @@ if(pet === 'yes' || pet === 'y'){
 }
 console.log('pet: ' + pet);
 
-
+//Question 5
 var favoriteCar = prompt('Do I have a dream car?');
 favoriteCar = favoriteCar.toLowerCase();
 
 if(favoriteCar === 'yes' || favoriteCar === 'y'){
   alert('Awesome! How did you know?');
+  counter++;
 } else if (favoriteCar === 'no' || favoriteCar === 'n'){
   alert('Sorry that is not correct :(');
 } else {
@@ -70,5 +78,52 @@ if(favoriteCar === 'yes' || favoriteCar === 'y'){
 }
 console.log('favoriteCar: ' + favoriteCar);
 
+//Question 6
+alert('I am thinking of a number between 1-10, I\'ll give you 4 tries to get it right. Let\'s do it');
+
+// allows user 4 tries to guess secret number
+for (var i = 0; i < 4; i++){
+  var guessNum = prompt('What number am I thinking of?');
+
+  if (guessNum <= 6) {
+    alert('Sorry that\'s too low');
+  } else if (guessNum >= 8){
+    alert('Sorry that is too high');
+  } else if (guessNum === 7 || guessNum === '7'){
+    alert('You got it!');
+    counter++;
+    break;
+  } else{
+    alert('This is not an acceptable answer');
+  }
+}
+alert('Seems like you ran out of tries :/ ');
+console.log('number Guessed: '+ guessNum);
 
 
+//Question 7
+var favoriteFood = ['pizza','hamburger','bread'];
+
+var userAnswer = prompt('One last question. What is my favorite thing to eat? Hint: This one has multiple answers').toLowerCase();
+
+var numOfAttepts = 0;
+
+// Source:https://stackoverflow.com/questions/183161/whats-the-best-way-to-break-from-nested-loops-in-javascript
+
+loop1:
+while(numOfAttepts < 5){
+  for (var j = 0; j < favoriteFood.length; j++){
+    if (userAnswer === favoriteFood[j]){
+      alert('You got it! here are all my favorite foods: ' + favoriteFood);
+      counter++;
+      break loop1;
+    }
+  }
+  userAnswer = prompt('Sorry this is wrong you have failed us all, Try again: ');
+  numOfAttepts++;
+}
+console.log('Favorite food: ' + favoriteFood);
+console.log('User answers: ' + userAnswer);
+console.log('Counter: ' + counter);
+
+alert('Here are the number of questions you got right ' + username +' '+ counter + ' out of 7');
